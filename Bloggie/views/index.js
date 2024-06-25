870d86dc7360b52e2074c125dccd0350fb4bcbea
TuralMuzafarov <turalmuzefferli4@gmail.com>
2024-06-25 19:09:42 +0400

    Form Page added in Bloggie

--- Bloggie/views/index.js
+++ Bloggie/views/index.js
@@ -17,16 +17,29 @@ function BlogObj(title, content, imagePath)
 
 let blogArray = [];
 
-// const __dirname = path.dirname(import.meta.dirname)
-// console.log(__dirname);
+const __dirname = path.dirname(import.meta.dirname)
+console.log(__dirname);
 
-// app.listen(port, () => {
-//     console.log(`App running on the port: ${port}`);
-// })
+app.listen(port, () => {
+    console.log(`App running on the port: ${port}`);
+})
 
-// app.use(express.static('../public'))
-// app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({extended : true}));
 
-// app.get('/', (req, res) => {
-//     res.render('index.ejs');
-// })
+app.use(express.static('../public'))
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    res.render('index.ejs');
+})
+
+
+app.get('/addBlog', (req, res) => {
+    res.sendFile('C:\\Users\\User\\Desktop\\Dr Anjela Yu\\Bloggie\\public\\html\\form.html');
+})
+
+app.post('/submitBlog', (req, res) => {
+    console.log(req.body);
+    res.redirect('/');
+})
+
